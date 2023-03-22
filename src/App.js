3d12b9b450beb141a0e7eee27a35cac3ed070a9a
its-pc <pujitachoudhary02@gmail.com>
2023-03-22 23:08:47 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import { useState } from 'react'
+import { useEffect } from 'react'
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
+import './App.css'
+import Voicecomponent from './component/Voicecomponent';
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    
+    <Voicecomponent/> 
+    <br>
+    </br>
+    <button className='btn btn-success'>processing</button>
+    
+    </>
+  )
 }
-
-export default App;
+export default App
